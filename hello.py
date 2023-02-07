28f3a5d4a33b8e29dcc8ba6f9cdc2f775ea35e1b
Dantul <danieltulus8@gmail.com>
2023-02-07 14:44:01 +0800

    .

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from markupsafe import escape
+# from flask import Flask
+# from markupsafe import escape
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/user/<username>")
-def show_user_profile(username):
-	return f'Welcome, {escape(username)}'
+# @app.route("/user/<username>")
+# def show_user_profile(username):
+# 	return f'Welcome, {escape(username)}'
 
-@app.route('/')
-def index():
-	return 'Index Page'
+# @app.route('/')
+# def index():
+# 	return 'Index Page'
